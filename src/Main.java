7a83a2f17add7995d1704027e3ae42f286c82566
Thiago <tjose.corp@gmail.com>
2025-08-19 21:20:07 -0300

    feat: implementar lógica principal para leitura de alunos e cálculo de aprovação

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,33 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import entities.Student;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner sc = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.print("Quantos aluno(a)s serão digitados? ");
+        int n = sc.nextInt();
+        sc.nextLine();
+        Student[] students = new Student[n];
+
+        for (int i = 0; i < students.length; i++) {
+            System.out.print("Informe o nome do Aluno(a): ");
+            String name = sc.nextLine();
+            System.out.print("Informe a nota da NP1: ");
+            double np1 = sc.nextDouble();
+            System.out.print("Informe a nota da NP2: ");
+            double np2 = sc.nextDouble();
+            sc.nextLine();
+            students[i] = new Student(name, np1, np2);
         }
+
+        System.out.println("\nAlunos aprovados: ");
+        for (Student s : students) {
+            if (s.getAverage() >= 6.0) {
+                System.out.printf("%s (Average: %.1f)\n", s.getName(), s.getAverage());
+            }
+        }
+        sc.close();
     }
 }
